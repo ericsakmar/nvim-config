c6b58f0a616f4736ed76c35cbea5405f17b0b7ff
Eric Sakmar <esakmar@truefit.io>
2024-01-17 16:01:18 -0500

    moves more stuff to files

--- lua/hacksaw/plugins/init.lua
+++ lua/hacksaw/plugins/init.lua
@@ -1,27 +1,10 @@
 return {
 	"nvim-lua/plenary.nvim",
-
 	"tpope/vim-fugitive",
 	"tpope/vim-repeat",
 	"tpope/vim-surround",
-
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup({
-				view = {
-					width = 50,
-				},
-			})
-		end,
-	},
-
 	"christoomey/vim-sort-motion", -- TODO replace this with something new?
+	"wcascades/vim-fugitive-toggle",
 
 	{
 		"numToStr/Comment.nvim",
@@ -30,8 +13,6 @@ return {
 		},
 		lazy = false,
 	},
-
-	"wcascades/vim-fugitive-toggle",
 
 	-- TODO consider
 	-- Plug 'folke/trouble.nvim'
