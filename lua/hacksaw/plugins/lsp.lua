cc652d20cf3a7458b28a71da75c8446203c0fc06
Eric Sakmar <esakmar@truefit.io>
2024-12-10 14:37:53 -0500

    before trying folke/snacks

--- lua/hacksaw/plugins/lsp.lua
+++ lua/hacksaw/plugins/lsp.lua
@@ -15,7 +15,7 @@ return {
 			require("mason-lspconfig").setup({
 				-- Replace the language servers listed here
 				-- with the ones you want to install
-				ensure_installed = { "tsserver", "rust_analyzer" },
+				ensure_installed = { "ts_ls", "rust_analyzer" },
 				handlers = {
 					lsp.default_setup,
 				},
